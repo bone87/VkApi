a9db3579249c3b1383b21f65a69c5d484717285e
bone <87bone@gmail.com>
2017-05-09 11:45:13 +0200

    edit index of testLikes

--- project/tests/testLikes.py
+++ project/tests/testLikes.py
@@ -6,7 +6,7 @@ from framework.support.commonFunctions import get_random_int
 from framework.support.log import log_info
 from project.api_call.friendsApi import FriendsApi
 from project.steps.likesSteps import add_like_to_profile_photo
-from project.steps.usersSteps import search_users_minsk
+from project.steps.usersSteps import search_users_minsk, search_users_lesnoi
 
 opera_account = '310582170'
 ff_account = '291495044'
@@ -40,14 +40,13 @@ def likes_friends(account_id):
                                            len=len(friends))
 
 
-def likes_users(account_id, age_from, age_to):
-    users = search_users_minsk(account_id=account_id,
-                               age_from=age_from,
-                               age_to=age_to)
+def likes_users(account_id):
+    users = search_users_lesnoi(account_id=account_id)
     log_info('Найдено {len}'.format(len=len(users)))
     count = 1
-    while count < 140:
-        random_index = random.randint(0, len(users))
+    while count < 140 or len(users) <= 1:
+        random_index = random.randint(0, len(users)-1)
+        print random_index, len(users)
         user = users.pop(random_index)
         if user.has_photo == 1:
             if add_like_to_profile_photo(account_id=account_id, user=user):
@@ -73,7 +72,7 @@ def likes_users(account_id, age_from, age_to):
 
 
 # likes_friends(ff_account)
-likes_users(opera_account, 25, 25)
+likes_users(ff_account)
 
 
 # list1 = list('spisok')
