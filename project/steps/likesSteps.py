f5c64d77ae5abbff7c4370eefa8170e5e5cb7c0f
e.bondarenko <87bone@gmail.com>
2017-05-09 19:57:27 +0300

    edit likeSteps

--- project/steps/likesSteps.py
+++ project/steps/likesSteps.py
@@ -3,7 +3,7 @@ import random
 
 import time
 
-from framework.support.commonFunctions import get_random_int
+from framework.support.commonFunctions import get_random_int, sleep
 from framework.support.log import log_info
 from project.api_call.friendsApi import FriendsApi
 from project.api_call.likesApi import LikesApi
@@ -48,15 +48,13 @@ def likes_users_photo_account(account_id, users):
             last_name=user.last_name))
         if add_like_to_profile_photo(account_id=account_id, user=user):
             random_seconds = get_random_int(150, 300)
-            log_info('  {message}: {count}, задержка {minutes:.2f} min.'.format(message='Лайк',
-                                                                                count=count,
-                                                                                minutes=float(random_seconds) / 60))
-            time.sleep(random_seconds)
+            log_info('  {message}: {count}.'.format(message='Лайк',
+                                                    count=count))
+            sleep(random_seconds)
             count += 1
         else:
             log_info('  [skip] Пользователь уже имеет лайк к фото.')
             random_seconds = get_random_int(2, 5)
-            log_info('      Задержка {seconds} sec.'.format(seconds=random_seconds))
-            time.sleep(random_seconds)
+            sleep(random_seconds)
     log_info('::: [END] Лайкнуто: {count}/{len} :::'.format(count=count - 1,
                                                             len=count_all_users_with_photo))
