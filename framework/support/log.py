4ee62ee6532ad870f1bd164425c9853abff95cc8
Bondarenko, Evgeny <e.bondarenko@a1qa.com>
2017-05-12 15:27:22 +0300

    log

--- framework/support/log.py
+++ framework/support/log.py
@@ -32,10 +32,10 @@ def config_log():
     format = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
     ch = logging.StreamHandler(sys.stdout)
     # ch.setFormatter(format)
-    log.addHandler(ch)
-    fh = handlers.RotatingFileHandler(create_log_file(), maxBytes=(1048576 * 5), backupCount=7)
-    fh.setFormatter(format)
-    log.addHandler(fh)
+    # log.addHandler(ch)
+    # fh = handlers.RotatingFileHandler(create_log_file(), maxBytes=(1048576 * 5), backupCount=7)
+    # fh.setFormatter(format)
+    # log.addHandler(fh)
     return log
 
 
