4228b657d1a5a8cc595dc48fe47fd2cb70f8a0bd
e.bondarenko <87bone@gmail.com>
2017-05-20 12:15:46 +0300

    add parallel message even

--- project/tests/parallel_send_birthday_message_even.py
+++ project/tests/parallel_send_birthday_message_even.py
@@ -32,24 +32,24 @@ def exec_commands(cmds):
 path_to_test1 = '/home/ITRANSITION.CORP/e.bondarenko/My_projects/VkApi/project/tests/send_birthday_message.robot'
 commands = [
     ['/home/ITRANSITION.CORP/e.bondarenko/.local/bin/pybot',
-        '--test', '+375298462344_Minsk_27',
-        '--output', 'output_375298462344_Minsk_27.xml',
-        '--report', 'report_375298462344_Minsk_27.html',
-        '--log', 'log_375298462344_Minsk_27.html', path_to_test1],
+        '--test', '+375298462344_Minsk_32',
+        '--output', 'output_375298462344_Minsk_32.xml',
+        '--report', 'report_375298462344_Minsk_32.html',
+        '--log', 'log_375298462344_Minsk_32.html', path_to_test1],
     ['/home/ITRANSITION.CORP/e.bondarenko/.local/bin/pybot',
-        '--test', '+375298360265_Minsk_28',
-        '--output', 'output_375298360265_Minsk_28.xml',
-        '--report', 'report_375298360265_Minsk_28.html',
-        '--log', 'log_375298360265_Minsk_28.html', path_to_test1],
+        '--test', '+375298360265_Minsk_33',
+        '--output', 'output_375298360265_Minsk_33.xml',
+        '--report', 'report_375298360265_Minsk_33.html',
+        '--log', 'log_375298360265_Minsk_33.html', path_to_test1],
 ]
 exec_commands(commands)
 print "Merge test reports"
 rebot('/home/ITRANSITION.CORP/e.bondarenko/My_projects/VkApi/project/tests/output_'
-      '375298462344_Minsk_27.xml',
+      '375298462344_Minsk_32.xml',
       '/home/ITRANSITION.CORP/e.bondarenko/My_projects/VkApi/project/tests/output_'
-      '375298360265_Minsk_28.xml',
+      '375298360265_Minsk_33.xml',
       report='/home/ITRANSITION.CORP/e.bondarenko/My_projects/VkApi/project/tests/reports/message'
-             '/report_send_birthdayMessage_evening.html',
+             '/report_send_birthday_message_evening.html',
       log='/home/ITRANSITION.CORP/e.bondarenko/My_projects/VkApi/project/tests/reports/message'
-          '/log_send_birthdayMessage_evening.html',
+          '/log_send_birthday_message_evening.html',
       xunit='/home/ITRANSITION.CORP/e.bondarenko/My_projects/VkApi/project/tests/reports/message/robotxunit.xml')
--- project/tests/send_birthday_message.robot
+++ project/tests/send_birthday_message.robot
@@ -8,18 +8,18 @@ ${opera_account} =  310582170
 ${ff_account} =  291495044
 
 *** Test Cases ***
-+375298462344_Minsk_25
-    ${users} =          Search Birthday Users            account_id=${opera_account}    age_from=25		    age_to=25
++375298462344_Minsk_30
+    ${users} =          Search Birthday Users            account_id=${opera_account}    age_from=30		    age_to=30
                         Send Birthday Messages           account_id=${opera_account}    users=${users}
 
-+375298360265_Minsk_26
-    ${users} =          Search Birthday Users            account_id=${ff_account}       age_from=26		    age_to=26
++375298360265_Minsk_31
+    ${users} =          Search Birthday Users            account_id=${ff_account}       age_from=31		    age_to=31
                         Send Birthday Messages           account_id=${ff_account}       users=${users}
 
-+375298462344_Minsk_27
-    ${users} =          Search Birthday Users            account_id=${opera_account}    age_from=27		    age_to=27
++375298462344_Minsk_32
+    ${users} =          Search Birthday Users            account_id=${opera_account}    age_from=32		    age_to=32
                         Send Birthday Messages           account_id=${opera_account}    users=${users}
 
-+375298360265_Minsk_28
-    ${users} =          Search Birthday Users            account_id=${ff_account}       age_from=28		    age_to=28
++375298360265_Minsk_33
+    ${users} =          Search Birthday Users            account_id=${ff_account}       age_from=33		    age_to=33
                         Send Birthday Messages           account_id=${ff_account}       users=${users}
