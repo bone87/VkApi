a8661415deb37923a3aa351bda7784b730c6b341
e.bondarenko <87bone@gmail.com>
2017-05-24 23:22:32 +0300

    edit parallel message

--- project/tests/parallel_send_birthday_message.py
+++ project/tests/parallel_send_birthday_message.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
+import os
 import time
 from subprocess import Popen, list2cmdline
 
+import sys
 from robot import rebot
 
 
@@ -29,41 +31,48 @@ def exec_commands(cmds):
             time.sleep(0.05)
 
 
-path_to_test1 = '/home/ITRANSITION.CORP/e.bondarenko/My_projects/VkApi/project/tests/send_birthday_message.robot'
+path_to_pybot_unix = '/home/ITRANSITION.CORP/e.bondarenko/.local/bin/pybot'
+path_to_pybot_win = 'c:/Python27/Scripts/pybot.bat'
+if sys.platform == 'win32':
+    path_to_pybot = path_to_pybot_win
+else:
+    path_to_pybot = path_to_pybot_unix
+path_to_tests = os.path.abspath(os.path.dirname(__file__))
+path_to_test1 = os.path.join(path_to_tests, 'send_birthday_message.robot')
+
 commands = [
-    ['/home/ITRANSITION.CORP/e.bondarenko/.local/bin/pybot',
-        '--test', '+375298681142_Minsk_26',
-        '--output', 'output_375298681142_Minsk_26.xml',
-        '--report', 'report_375298681142_Minsk_26.html',
-        '--log', 'log_375298681142_Minsk_26.html', path_to_test1],
-    ['/home/ITRANSITION.CORP/e.bondarenko/.local/bin/pybot',
-        '--test', '+375447693824_Minsk_27',
-        '--output', 'output_375447693824_Minsk_27.xml',
-        '--report', 'report_375447693824_Minsk_27.html',
-        '--log', 'log_375447693824_Minsk_27.html', path_to_test1],
-    ['/home/ITRANSITION.CORP/e.bondarenko/.local/bin/pybot',
-        '--test', '+375298462344_Minsk_30',
-        '--output', 'output_375298462344_Minsk_30.xml',
-        '--report', 'report_375298462344_Minsk_30.html',
-        '--log', 'log_375298462344_Minsk_30.html', path_to_test1],
-    ['/home/ITRANSITION.CORP/e.bondarenko/.local/bin/pybot',
-        '--test', '+375298360265_Minsk_31',
-        '--output', 'output_375298360265_Minsk_31.xml',
-        '--report', 'report_375298360265_Minsk_31.html',
-        '--log', 'log_375298360265_Minsk_31.html', path_to_test1],
+    [path_to_pybot,
+        '--test',       '375298462344_Minsk_mes_31',
+        '--output',     'reports/message/tmp/output_375298462344_Minsk_mes_31.xml',
+        '--report',     'reports/message/tmp/report_375298462344_Minsk_mes_31.html',
+        '--log',        'reports/message/tmp/log_375298462344_Minsk_mes_31.html', path_to_test1],
+    [path_to_pybot,
+        '--test',       '375298360265_Minsk_mes_30',
+        '--output',     'reports/message/tmp/output_375298360265_Minsk_mes_30.xml',
+        '--report',     'reports/message/tmp/report_375298360265_Minsk_mes_30.html',
+        '--log',        'reports/message/tmp/log_375298360265_Minsk_mes_30.html', path_to_test1],
+    [path_to_pybot,
+        '--test',       '375298681142_Minsk_mes_29',
+        '--output',     'reports/message/tmp/output_375298681142_Minsk_mes_29.xml',
+        '--report',     'reports/message/tmp/report_375298681142_Minsk_mes_29.html',
+        '--log',        'reports/message/tmp/log_375298681142_Minsk_mes_29.html', path_to_test1],
+    [path_to_pybot,
+        '--test',       '375447693824_Minsk_mes_28',
+        '--output',     'reports/message/tmp/output_375447693824_Minsk_mes_28.xml',
+        '--report',     'reports/message/tmp/report_375447693824_Minsk_mes_28.html',
+        '--log',        'reports/message/tmp/log_375447693824_Minsk_mes_28.html', path_to_test1],
+    [path_to_pybot,
+        '--test',       '375445528788_Minsk_mes_27',
+        '--output',     'reports/message/tmp/output_375445528788_Minsk_mes_27.xml',
+        '--report',     'reports/message/tmp/report_375445528788_Minsk_mes_27.html',
+        '--log',        'reports/message/tmp/log_375445528788_Minsk_mes_27.html', path_to_test1],
 ]
 exec_commands(commands)
-print "Merge test reports"
-rebot('/home/ITRANSITION.CORP/e.bondarenko/My_projects/VkApi/project/tests/output_'
-      '375298462344_Minsk_30.xml',
-      '/home/ITRANSITION.CORP/e.bondarenko/My_projects/VkApi/project/tests/output_'
-      '375298360265_Minsk_31.xml',
-      '/home/ITRANSITION.CORP/e.bondarenko/My_projects/VkApi/project/tests/output_'
-      '375298681142_Minsk_26.xml',
-      '/home/ITRANSITION.CORP/e.bondarenko/My_projects/VkApi/project/tests/output_'
-      '375447693824_Minsk_27.xml',
-      report='/home/ITRANSITION.CORP/e.bondarenko/My_projects/VkApi/project/tests/reports/message'
-             '/report_send_birthdayMessage_morning.html',
-      log='/home/ITRANSITION.CORP/e.bondarenko/My_projects/VkApi/project/tests/reports/message'
-          '/log_send_birthdayMessage_morning.html',
-      xunit='/home/ITRANSITION.CORP/e.bondarenko/My_projects/VkApi/project/tests/reports/message/robotxunit.xml')
+rebot(os.path.join(path_to_tests,           'reports/message/tmp/output_375298462344_Minsk_mes_31.xml'),
+      os.path.join(path_to_tests,           'reports/message/tmp/output_375298360265_Minsk_mes_30.xml'),
+      os.path.join(path_to_tests,           'reports/message/tmp/output_375298681142_Minsk_mes_29.xml'),
+      os.path.join(path_to_tests,           'reports/message/tmp/output_375447693824_Minsk_mes_28.xml'),
+      os.path.join(path_to_tests,           'reports/message/tmp/output_375445528788_Minsk_mes_27.xml'),
+      report=os.path.join(path_to_tests,    'reports/message/report_send_birthday_message.html'),
+      log=os.path.join(path_to_tests,       'reports/message/log_send_birthday_message.html'),
+      xunit=os.path.join(path_to_tests,     'reports/message/robotxunit.xml'))
--- project/tests/send_birthday_message.robot
+++ project/tests/send_birthday_message.robot
@@ -8,42 +8,30 @@ ${opera_account} =  310582170
 ${ff_account} =  291495044
 ${ff_account_375298681142} =  286454976
 ${ff_account_375447693824} =  286454510
+${ff_account_375445528788} =  280826200
 
 *** Test Cases ***
-+375298681142_Minsk_26
-    ${users} =          Search Birthday Users            account_id=${ff_account_375298681142}    age_from=26	        age_to=26
-                        Send Birthday Messages           account_id=${ff_account_375298681142}    users=${users}
+375298462344_Minsk_mes_31
+    ${users} =               Search Birthday Users            account_id=${opera_account}                    age_from=31		    age_to=31
+                             Send Birthday Messages           account_id=${opera_account}                    users=${users}
+
+375298360265_Minsk_mes_30
+    ${users} =               Search Birthday Users            account_id=${ff_account}                       age_from=30		    age_to=30
+                             Send Birthday Messages           account_id=${ff_account}                       users=${users}
+
+375298681142_Minsk_mes_29
+    ${users} =               Search Birthday Users            account_id=${ff_account_375298681142}          age_from=29	        age_to=29
+                             Send Birthday Messages           account_id=${ff_account_375298681142}          users=${users}
 
 
-+375447693824_Minsk_27
-    ${users} =          Search Birthday Users            account_id=${ff_account_375447693824}    age_from=27		    age_to=27
-                        Send Birthday Messages           account_id=${ff_account_375447693824}    users=${users}
-
-
-+375298681142_Minsk_28
-    ${users} =          Search Birthday Users            account_id=${ff_account_375298681142}    age_from=28		    age_to=28
-                        Send Birthday Messages           account_id=${ff_account_375298681142}    users=${users}
-
-
-+375447693824_Minsk_29
-    ${users} =          Search Birthday Users            account_id=${ff_account_375447693824}    age_from=29		    age_to=29
-                        Send Birthday Messages           account_id=${ff_account_375447693824}    users=${users}
-
+375447693824_Minsk_mes_28
+    ${users} =               Search Birthday Users            account_id=${ff_account_375447693824}          age_from=28		    age_to=28
+                             Send Birthday Messages           account_id=${ff_account_375447693824}          users=${users}
 
 
 
-+375298462344_Minsk_30
-    ${users} =          Search Birthday Users            account_id=${opera_account}    age_from=30		    age_to=30
-                        Send Birthday Messages           account_id=${opera_account}    users=${users}
+375445528788_Minsk_mes_27
+    ${users} =               Search Birthday Users            account_id=${ff_account_375445528788}          age_from=27		    age_to=27
+                             Send Birthday Messages           account_id=${ff_account_375445528788}          users=${users}
 
-+375298360265_Minsk_31
-    ${users} =          Search Birthday Users            account_id=${ff_account}       age_from=31		    age_to=31
-                        Send Birthday Messages           account_id=${ff_account}       users=${users}
 
-+375298462344_Minsk_32
-    ${users} =          Search Birthday Users            account_id=${opera_account}    age_from=32		    age_to=32
-                        Send Birthday Messages           account_id=${opera_account}    users=${users}
-
-+375298360265_Minsk_33
-    ${users} =          Search Birthday Users            account_id=${ff_account}       age_from=33		    age_to=33
-                        Send Birthday Messages           account_id=${ff_account}       users=${users}
