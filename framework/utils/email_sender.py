440ad9358a8e11c57b01ff8831c6e03c2e8fcdc3
e.bondarenko <87bone@gmail.com>
2017-07-01 13:55:36 +0300

    edit email_sender

--- framework/utils/email_sender.py
+++ framework/utils/email_sender.py
@@ -9,9 +9,10 @@ import datetime
 
 def send_email_with_attach(subject, attached_file, recipien="87bone@gmail.com",):
     msg = MIMEMultipart()
+    msg['From'] = 'useru009@gmail.com'
+    msg['To'] = recipien
     msg['Subject'] = '{date}_{subject}'.format(date=datetime.datetime.now().strftime("%Y-%m-%d"),
                                                subject=subject)
-    msg['From'] = 'useru009@gmail.com'
     msg.preamble = 'Multipart massage.\n'
     part = MIMEApplication(open(str(attached_file), "rb").read())
     part.add_header('Content-Disposition', 'attachment', filename=str(attached_file))
