ce0168e6adbabc12bed59d42376888e0737bf039
e.bondarenko <87bone@gmail.com>
2017-07-02 20:43:51 +0300

    edit path

--- project/tests/parallel_addLikes.py
+++ project/tests/parallel_addLikes.py
@@ -44,47 +44,47 @@ else:
     path_to_pybot = path_to_pybot_unix
 path_to_tests = os.path.abspath(os.path.dirname(__file__))
 path_to_test1 = os.path.join(path_to_tests, 'add_likes.robot')
-path_to_output = os.path.join(path_to_tests, 'reports/likes/tmp/')
+path_to_output = os.path.join(path_to_tests, 'reports{sep}likes{sep}tmp{sep}'.format(sep=os.sep))
 
 commands = [
     [path_to_pybot,
      '--test', '375298681142_Minsk_likes',
-     '--output', os.path.join(path_to_tests, 'output_375298681142_Minsk_likes.xml'),
+     '--output', os.path.join(path_to_output, 'output_375298681142_Minsk_likes.xml'),
      path_to_test1],
 
     [path_to_pybot,
      '--test', '375447693824_Minsk_likes',
-     '--output', os.path.join(path_to_tests, 'output_375447693824_Minsk_likes.xml'),
+     '--output', os.path.join(path_to_output, 'output_375447693824_Minsk_likes.xml'),
      path_to_test1],
 
     [path_to_pybot,
      '--test', '375298462344_Minsk_likes',
-     '--output', os.path.join(path_to_tests, 'output_375298462344_Minsk_likes.xml'),
+     '--output', os.path.join(path_to_output, 'output_375298462344_Minsk_likes.xml'),
      path_to_test1],
 
     [path_to_pybot,
      '--test', '375298360265_Minsk_likes',
-     '--output', os.path.join(path_to_tests, 'output_375298360265_Minsk_likes.xml'),
+     '--output', os.path.join(path_to_output, 'output_375298360265_Minsk_likes.xml'),
      path_to_test1],
 
     [path_to_pybot,
      '--test', '375445528788_Minsk_likes',
-     '--output', os.path.join(path_to_tests, 'output_375445528788_Minsk_likes.xml'),
+     '--output', os.path.join(path_to_output, 'output_375445528788_Minsk_likes.xml'),
      path_to_test1],
 
     [path_to_pybot,
      '--test', '375292025693_Minsk_likes',
-     '--output', os.path.join(path_to_tests, 'output_375292025693_Minsk_likes.xml'),
+     '--output', os.path.join(path_to_output, 'output_375292025693_Minsk_likes.xml'),
      path_to_test1],
 
     [path_to_pybot,
      '--test', 'Bone_Minsk_likes_engaged',
-     '--output', os.path.join(path_to_tests, 'output_Bone_Minsk_likes_engaged.xml'),
+     '--output', os.path.join(path_to_output, 'output_Bone_Minsk_likes_engaged.xml'),
      path_to_test1],
 
     [path_to_pybot,
      '--test', 'Prichello_Minsk_likes_engaged',
-     '--output', os.path.join(path_to_tests, 'output_Prichello_Minsk_likes_engaged.xml'),
+     '--output', os.path.join(path_to_output, 'output_Prichello_Minsk_likes_engaged.xml'),
      path_to_test1]
 ]
 exec_commands(commands)
@@ -96,11 +96,14 @@ rebot(os.path.join(path_to_output, 'output_375298681142_Minsk_likes.xml'),
       os.path.join(path_to_output, 'output_375292025693_Minsk_likes.xml'),
       os.path.join(path_to_output, 'output_Bone_Minsk_likes_engaged.xml'),
       os.path.join(path_to_output, 'output_Prichello_Minsk_likes_engaged.xml'),
-      report=os.path.join(path_to_tests, 'reports/likes/report_{pref_data}_add_likes.html'.format(
+      report=os.path.join(path_to_output, '..{sep}report_{pref_data}_add_likes.html'.format(
+          sep=os.sep,
           pref_data=datetime.datetime.now().strftime("%Y-%m-%d"))),
-      log=os.path.join(path_to_tests, 'reports/likes/log_{pref_data}_add_likes.html'.format(
+      log=os.path.join(path_to_tests, '..{sep}log_{pref_data}_add_likes.html'.format(
+          sep=os.sep,
           pref_data=datetime.datetime.now().strftime("%Y-%m-%d"))),
-      xunit=os.path.join(path_to_tests, 'reports/likes/robotxunit_{pref_data}_add_likes.xml').format(
+      xunit=os.path.join(path_to_tests, '..{sep}robotxunit_{pref_data}_add_likes.xml').format(
+          sep=os.sep,
           pref_data=datetime.datetime.now().strftime("%Y-%m-%d")))
 # send_email_with_attach("add_likes",
 #                        os.path.join(path_to_tests, 'reports/likes/log_{pref_data}_add_likes.html'.format(
