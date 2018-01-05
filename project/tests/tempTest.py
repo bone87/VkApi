acba86ee37ecbb3d2e03990b1d03f24a60003b12
bone <87bone@gmail.com>
2018-01-05 13:40:15 +0300

    upd

--- project/tests/tempTest.py
+++ project/tests/tempTest.py
@@ -6,8 +6,9 @@ from project.steps.messagesSteps import send_birthday_messages
 from project.steps.usersSteps import search_birthday_users
 
 numbers_tokens = [
-    # ["25 7214755", "f325c6364f158a6c2b21a1e9272c605dfb167171369f3390d065dfc247f7a607f903af0b7e3511a538f19", 10],
+    ["25 7214755", "f325c6364f158a6c2b21a1e9272c605dfb167171369f3390d065dfc247f7a607f903af0b7e3511a538f19", 10],
     ["25 9071166", "340b0643780bd0c3f5b69e2442136a6fbecdbbebfaf50d0a191125f0a4ad4a464513737d4ae0fd03b26e7", 10]
+    # ["25 9065400", "f041569b15d8fb4dbd6d8075e25aea4dd23ce24def7f2a2adf21993a2a4bae41d336fbaa0da3866833e8f", 10]
 ]
 
 
