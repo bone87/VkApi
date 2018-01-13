58c0798e6b00b8226b43468935e88909645992b0
bone <87bone@gmail.com>
2018-01-13 13:19:56 +0300

    25 7544404

--- project/tests/tempTest.py
+++ project/tests/tempTest.py
@@ -7,9 +7,9 @@ from project.steps.usersSteps import search_birthday_users
 
 numbers_tokens = [
     ["25 7214755", "a3383acc5f71ac8b6b0e88184f10b0084625ab832f84f3e72fb20e6e858b1c2b11f9f98ffca7e011bd398", get_random_int(10, 15)],
-    ["25 9071166", "340b0643780bd0c3f5b69e2442136a6fbecdbbebfaf50d0a191125f0a4ad4a464513737d4ae0fd03b26e7", get_random_int(9, 15)],
-    ["25 9065400", "f041569b15d8fb4dbd6d8075e25aea4dd23ce24def7f2a2adf21993a2a4bae41d336fbaa0da3866833e8f", get_random_int(9, 15)],
-    ["25 9062961", "249e99f5fd6bc66baabc11d4349576fa762e1c20cba284ae1215c54204e045b52cd760e1ed37a4459eaa3", get_random_int(9, 15)],
+    ["25 9071166", "340b0643780bd0c3f5b69e2442136a6fbecdbbebfaf50d0a191125f0a4ad4a464513737d4ae0fd03b26e7", get_random_int(7, 12)],
+    ["25 9065400", "f041569b15d8fb4dbd6d8075e25aea4dd23ce24def7f2a2adf21993a2a4bae41d336fbaa0da3866833e8f", get_random_int(7, 12)],
+    # ["25 9062961", "ed9694b71bcd0a51d2c907fb0ef2163a4d99201ffa0c2a0287ea5a18c2cc44580a2731ba2e26e9018ea5f", get_random_int(9, 15)],
     ["25 9068942", "ccae722d41b078eea221ba7364c0aa47f4cc7f727a5d3e82f5261ca6764f4b49088e2238ff6122addf73c", get_random_int(8, 10)],
     ["25 9056798", "7ceb6dc02053c584513027d790b960fa5ef9581c6d11a7a54bbb9f3fb2e8ad590dff1c67234cf63b5a09b", get_random_int(8, 10)],
     ["25 7632584", "998aac3cf32cbf901a98720e7091839c6b708fd95535bb52b4c2ad55ad3c83278b194d59bd7b247056631", get_random_int(7, 11)],
